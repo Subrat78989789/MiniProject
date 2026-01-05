a4e04ee0fb726461d96536e3c1fcd73b5b91698b
Subrat Gupta <guptasubrat111@gmail.com>
2026-01-05 15:19:12 +0530

    Mini Project initial Commit.

---
+++ src/consolebank/FileUtil.java
@@ -0,0 +1,22 @@
+package consolebank;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class FileUtil {
+	
+	public static void writeAccountdetails(Account ac)throws Exception
+	{
+		File f=new File("Bank.txt");
+		f.createNewFile();
+		FileWriter fWriter=new FileWriter(f,true);
+		if(f.canWrite())
+		{
+			fWriter.write(ac.holderName+" | "+ac.accNo+" | "+ac.balance+" | "+ac.caller+"\n");
+		}
+		fWriter.close();
+		
+		
+	}
+
+}
